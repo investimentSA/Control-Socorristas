20db087a2c859dbd23e89e82f1468972cf0c6a89
investimentSA <wbtpaul@gmail.com>
2025-02-04 10:00:54 +0100

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-import { auth, db } from './firebase.js'; // Importamos las funciones de Firebase
+// app.js
+
+import { auth, db } from './firebase.js'; // Importa las funciones necesarias de Firebase
 
 const loginBtn = document.getElementById('login-btn');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const errorMessage = document.getElementById('error-message');
 
+// Iniciar sesión
 loginBtn.addEventListener('click', async (e) => {
-    e.preventDefault(); // Evitar que el formulario se envíe automáticamente
+    e.preventDefault(); // Evitar el comportamiento predeterminado del formulario
 
     const email = emailInput.value;
     const password = passwordInput.value;
 
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        // Si todo está bien, redirigimos a la página principal o mostramos el mensaje de éxito
+        // Si todo está bien, muestra un mensaje de éxito
         console.log('Usuario logueado', userCredential.user);
     } catch (error) {
+        // Si hay un error, muestra el mensaje de error
         errorMessage.textContent = 'Error de autenticación: ' + error.message;
     }
 });
