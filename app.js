94426e12d40353ed2c525da4f98255e9d5107493
investimentSA <wbtpaul@gmail.com>
2025-02-05 05:53:33 +0100

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const supabaseUrl = 'https://lgvmxoamdxbhtmicawlv.supabase.co';
-    const supabaseKey = 'TU_SUPABASE_KEY_AQUÍ'; // ⚠️ No expongas tu clave en producción
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxndm14b2FtZHhiaHRtaWNhd2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2NjA0NDIsImV4cCI6MjA1NDIzNjQ0Mn0.0HpIAqpg3gPOAe714dAJPkWF8y8nQBOK7_zf_76HFKw'; // ⚠️ No expongas tu clave en producción
     const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
     let hasClockedIn = localStorage.getItem('hasClockedIn') === 'true';
